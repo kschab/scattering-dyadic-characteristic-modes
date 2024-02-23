6b36295d8e2678e75a3cfbe1622098c6f58a6487
Miloslav_Capek <miloslav.capek@fel.cvut.cz>
2024-02-23 22:21:11 +0100

    Remove old version without substructures.

--- FEM_MoM-FEKO/+feko/cleanupFEKOcurrents.m
+++
@@ -1,8 +0,0 @@
-function cleanupFEKOcurrents(preFEKOfileName)
-% CLEANUPFEKOCURRENTS: clean all temporary FEKO files related to currents
-% 
-% (c) 2022, Miloslav Capek, CTU in Prague, miloslav.capek@fel.cvut.cz
-
-try
-    delete([preFEKOfileName '_Currents*.*']);
-end
--- FEM_MoM-FEKO/+feko/cleanupFEKOfarfieldFiles.m
+++
@@ -1,9 +0,0 @@
-function cleanupFEKOfarfieldFiles(preFEKOfileName)
-% CLEANUPFEKOFARFIELDFILES: clean all temporary FEKO files related to far
-% fields
-% 
-% (c) 2022, Miloslav Capek, CTU in Prague, miloslav.capek@fel.cvut.cz
-
-try
-    delete([preFEKOfileName '_FarField*.ffe']);
-end
